a1480fdadff9a70a0b7706aa0ea6d4630f334262
farham aghdasi <farhamaghdasi08@gmail.com>
2025-08-30 12:23:10 +0430

    fix little bug in .bat

--- ai-get.bat
+++ ai-get.bat
@@ -9,7 +9,7 @@ cls
 title Python Collect Data
 
 :: Run the main.py script
-python "D:\$RTL-THEMES Apps\python-ai-data\main.py"
+python "main.py"
 
 :: Pause the script to keep the window open after execution
 pause
